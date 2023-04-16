b9a5cdd3abbe9e28d5b6d711652da64c661a3121
AlpacaMoon <thongxsgaming@gmail.com>
2023-04-16 15:23:16 +0800

    struct

--- Assignment.sol
+++ Assignment.sol
@@ -1,14 +1,24 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-//try commit
 contract Durian {
-    
-    enum Stage {
-        AtFarm,
-        AtDistributionCenter, 
-        AtRetailer, 
-        AtCustomer
+    struct worker{
+        
     }
-    // test
+
+    struct durianTree{
+
+    }
+
+    struct durianFarm{
+
+    }
+
+    struct durianFarmSector{
+        
+    }
+
+    constructor() {
+
+    }
 }
