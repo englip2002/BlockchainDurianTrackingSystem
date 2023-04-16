856e2e53c04cc304ec9cbfb580a56ad9fa6e984b
Eng Lip <englip455@gmail.com>
2023-04-16 15:29:58 +0800

    struct

--- Assignment.sol
+++ Assignment.sol
@@ -3,19 +3,36 @@ pragma solidity ^0.8.0;
 
 contract Durian {
     struct worker{
+        string workerID;
         
     }
 
     struct durianTree{
-
+        string treeID;
+        durianFarm farm;
+        durianFarmSector farmSector;
+        uint256 age;
+        string species;
+        uint256 lastHarvestTime;
     }
 
     struct durianFarm{
-
+        string farmID;
+        string name;
+        string location;
+        durianFarmSector[] sectors;
     }
 
     struct durianFarmSector{
-        
+        string farmSectorID;
+        uint256 duringTreeCount;
+    }
+
+    enum Stage {
+        Init,
+        Reg,
+        Vote,
+        Done
     }
 
     constructor() {
