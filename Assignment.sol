89535b52fd9dc1f945b10301d8cafe7a2c3fc88a
AlpacaMoon <thongxsgaming@gmail.com>
2023-04-16 15:34:42 +0800

    mychanges

--- Assignment.sol
+++ Assignment.sol
@@ -2,7 +2,45 @@
 pragma solidity ^0.8.0;
 
 contract Durian {
+    uint256 id;
+    uint256 weightGrams;
+    string species;
+    
+    // Timestamps for the four stages
+    uint[] stageTimestamps;
+    
+    enum Stages {
+        Harvested, 
+        AtDistributionCenter, 
+        AtRetailer, 
+        SoldToCustomer
+    }
+
+    enum Rate {
+        Worst, 
+        Bad, 
+        Normal, 
+        Good, 
+        Excellent
+    }
+
+    // Ratings given to this durian by the customers
+    rating[] ratings;
+
+    struct rating {
+        address ratingCustomer;
+        Rate taste;
+        Rate fragrance;
+        Rate creaminess;
+        Rate price; 
+    }
+
+
     struct worker{
+        
+    }
+
+    struct Customer {
         
     }
 
