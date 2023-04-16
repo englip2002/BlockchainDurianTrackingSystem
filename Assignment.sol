dee4ef70a2281cebe1e3870945902f42b2a57ae6
englip2002 <englip455@gmail.com>
2023-04-16 15:15:06 +0800

    durian

--- Assignment.sol
+++ Assignment.sol
@@ -2,5 +2,23 @@
 pragma solidity ^0.8.0;
 
 contract Durian {
-    
+    struct worker{
+        
+    }
+
+    struct durianTree{
+
+    }
+
+    struct durianFarm{
+
+    }
+
+    struct durianFarmSector{
+        
+    }
+
+    constructor() {
+
+    }
 }
