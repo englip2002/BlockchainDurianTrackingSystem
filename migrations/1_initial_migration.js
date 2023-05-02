670a04ee74bef1d22d98ab2846c88110adc960f1
AlpacaMoon <thongxsgaming@gmail.com>
2023-05-02 09:19:22 +0800

    delete all MDFK

--- migrations/1_initial_migration.js
+++
@@ -1,7 +0,0 @@
-var Migrations = artifacts.require("./Migrations.sol");
-var DTTBA = artifacts.require("./DTTBA.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(Migrations);
-  deployer.deploy(DTTBA);
-};
